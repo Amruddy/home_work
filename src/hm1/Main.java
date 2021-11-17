1b4513afa3ffb407f580d51ff2ac9bfdb0eeb5b6
Amruddy <embaev94@gmail.com>
2021-11-17 21:43:10 +0300

    Initial commit

---
+++ src/hm1/Main.java
@@ -0,0 +1,18 @@
+package hm1;
+
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+
+    }
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+
+       
+
+   }
+
+}
